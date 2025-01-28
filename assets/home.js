589064923756b8f71a5034a40bf1f29e9c617356
evanlihou <evan@evanlihou.com>
2025-01-28 08:40:11 -0500

    Scope whole script and guard against double initialization

--- assets/home.js
+++ assets/home.js
@@ -1,110 +1,115 @@
-const LOUDNESS_MULTIPLIER = 3;
-const SEEK_SECONDS = 3;
+(function () {
+    if (typeof window.initialized !== 'undefined') return;
+    window.initialized = true;
 
-window.currentFile = null;
+    const LOUDNESS_MULTIPLIER = 3;
+    const SEEK_SECONDS = 3;
 
-const player = document.getElementById("player");
-const autoplayToggle = document.getElementById("autoplayToggle");
+    window.currentFile = null;
 
-if (navigator && navigator.audioSession)
-    navigator.audioSession.type = "playback";
+    const player = document.getElementById("player");
+    const autoplayToggle = document.getElementById("autoplayToggle");
 
-function setAudioSource(e) {
-    player.parentElement.style.display = "block";
-    player.src = '/file/' + e.target.dataset["path"];
-    player.play();
-    context.resume();
-    window.currentFile = e.target.dataset["path"];
-    document.querySelectorAll("a[data-active]").forEach(el => delete el.dataset["active"]);
-}
+    if (navigator && navigator.audioSession)
+        navigator.audioSession.type = "playback";
 
-document.getElementById("fileListing").addEventListener('htmx:load', function() {
-    if (window.currentFile === null) return;
-    let el = document.querySelector("a[data-path=\"" + window.currentFile + "\"]");
-    if (el === null) return;
-    el.dataset["active"] = "true";
-});
+    window.setAudioSource = function(e) {
+        player.parentElement.style.display = "block";
+        player.src = '/file/' + e.target.dataset["path"];
+        player.play();
+        context.resume();
+        window.currentFile = e.target.dataset["path"];
+        document.querySelectorAll("a[data-active]").forEach(el => delete el.dataset["active"]);
+    }
 
-function getFileEls() {
-    return Array.from(document.querySelectorAll("#fileListing a[data-path]"));
-}
-
-function changeAudio(el) {
-    el.scrollIntoView({
-        block: "nearest",
-        behavior: "smooth"
+    document.getElementById("fileListing").addEventListener('htmx:load', function() {
+        if (window.currentFile === null) return;
+        let el = document.querySelector("a[data-path=\"" + window.currentFile + "\"]");
+        if (el === null) return;
+        el.dataset["active"] = "true";
     });
-    document.querySelectorAll("#fileListing a[data-active]").forEach(el => delete el.dataset["active"])
-    el.click();
-}
 
-function selectRelativeFile(deltaIndex) {
-    const files = getFileEls();
-    const activeIndex = files.findIndex(f => f.hasAttribute("data-active"));
-    const newIndex = activeIndex + deltaIndex;
+    function getFileEls() {
+        return Array.from(document.querySelectorAll("#fileListing a[data-path]"));
+    }
 
-    if (newIndex < 0 || newIndex >= files.length) return;
+    function changeAudio(el) {
+        el.scrollIntoView({
+            block: "nearest",
+            behavior: "smooth"
+        });
+        document.querySelectorAll("#fileListing a[data-active]").forEach(el => delete el.dataset["active"])
+        el.click();
+    }
 
-    changeAudio(files[activeIndex + deltaIndex]);
-}
+    function selectRelativeFile(deltaIndex) {
+        const files = getFileEls();
+        const activeIndex = files.findIndex(f => f.hasAttribute("data-active"));
+        const newIndex = activeIndex + deltaIndex;
 
-document.addEventListener("keydown", (e) => {
-    if (e.shiftKey || e.altKey || e.ctrlKey || e.metaKey) return;
-    if (e.key === "ArrowDown" || e.key === "ArrowUp") {
-        e.preventDefault();
+        if (newIndex < 0 || newIndex >= files.length) return;
 
-        if (e.key === "ArrowDown") {
-            selectRelativeFile(1);
-        } else if (e.key === "ArrowUp") {
-            selectRelativeFile(-1);
-        }
-    } else if (e.key === "ArrowLeft" || e.key === "ArrowRight") {
-        e.preventDefault();
-        if (player === null || player.currentSrc === "") return;
-        if (e.key === "ArrowLeft") {
-            player.currentTime = player.currentTime - SEEK_SECONDS;
+        changeAudio(files[activeIndex + deltaIndex]);
+    }
+
+    document.addEventListener("keydown", (e) => {
+        if (e.shiftKey || e.altKey || e.ctrlKey || e.metaKey) return;
+        if (e.key === "ArrowDown" || e.key === "ArrowUp") {
+            e.preventDefault();
+
+            if (e.key === "ArrowDown") {
+                selectRelativeFile(1);
+            } else if (e.key === "ArrowUp") {
+                selectRelativeFile(-1);
+            }
+        } else if (e.key === "ArrowLeft" || e.key === "ArrowRight") {
+            e.preventDefault();
+            if (player === null || player.currentSrc === "") return;
+            if (e.key === "ArrowLeft") {
+                player.currentTime = player.currentTime - SEEK_SECONDS;
+                if (player.paused) {
+                    player.play();
+                    context.resume();
+                }
+            } else if (e.key === "ArrowRight") {
+                player.currentTime = player.currentTime + SEEK_SECONDS;
+            }
+        } else if (e.key === " ") {
+            e.preventDefault();
+
+            if (player === null || player.currentSrc === "") return;
+
             if (player.paused) {
                 player.play();
                 context.resume();
+            } else {
+                player.pause();
             }
-        } else if (e.key === "ArrowRight") {
-            player.currentTime = player.currentTime + SEEK_SECONDS;
+        } else if (e.key === "a") {
+            e.preventDefault();
+
+            autoplayToggle.click();
         }
-    } else if (e.key === " ") {
+    });
+
+    const context = new AudioContext();
+    const source = context.createMediaElementSource(player);
+    const gain = context.createGain();
+    source.connect(gain);
+    gain.connect(context.destination);
+    gain.gain.value = LOUDNESS_MULTIPLIER;
+
+    autoplayToggle.addEventListener("click", (e) => {
         e.preventDefault();
 
-        if (player === null || player.currentSrc === "") return;
+        autoplayToggle.dataset["value"] = autoplayToggle.dataset["value"] === "true" ? "false" : "true";
 
-        if (player.paused) {
-            player.play();
-            context.resume();
-        } else {
-            player.pause();
-        }
-    } else if (e.key === "a") {
-        e.preventDefault();
+        autoplayToggle.innerText = autoplayToggle.dataset["value"] === "true" ? "Autoplay ON" : "Autoplay OFF";
+    });
 
-        autoplayToggle.click();
-    }
-});
+    document.getElementById("player").addEventListener("ended", () => {
+        if (autoplayToggle.dataset["value"] !== "true") return;
 
-const context = new AudioContext();
-const source = context.createMediaElementSource(player);
-const gain = context.createGain();
-source.connect(gain);
-gain.connect(context.destination);
-gain.gain.value = LOUDNESS_MULTIPLIER;
-
-autoplayToggle.addEventListener("click", (e) => {
-    e.preventDefault();
-
-    autoplayToggle.dataset["value"] = autoplayToggle.dataset["value"] === "true" ? "false" : "true";
-
-    autoplayToggle.innerText = autoplayToggle.dataset["value"] === "true" ? "Autoplay ON" : "Autoplay OFF";
-});
-
-document.getElementById("player").addEventListener("ended", () => {
-    if (autoplayToggle.dataset["value"] !== "true") return;
-
-    selectRelativeFile(1);
-});
+        selectRelativeFile(1);
+    });
+})();
